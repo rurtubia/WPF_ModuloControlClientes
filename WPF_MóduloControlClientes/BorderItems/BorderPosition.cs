4c2992613abdbe0b9960653636a52c5a94f339d1
rurtubiac <randy.urtubia@inacapmail.cl>
2015-06-29 09:13:18 -0300

    Added Resizable Window Classes, to LoginWindow: Stackpanels, resizing methods.

---
+++ WPF_MóduloControlClientes/BorderItems/BorderPosition.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_MóduloControlClientes
+{
+    public enum BorderPosition
+    {
+        Left = 61441,
+        Right = 61442,
+        Top = 61443,
+        TopLeft = 61444,
+        TopRight = 61445,
+        Bottom = 61446,
+        BottomLeft = 61447,
+        BottomRight = 61448
+    }
+}
---
+++ WPF_MóduloControlClientes/BorderItems/WindowBorder.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+
+namespace WPF_MóduloControlClientes
+{
+    public class WindowBorder
+    {
+        /// <summary>
+        /// The element which is acting as the border.
+        /// </summary>
+        public FrameworkElement Element { get; private set; }
+
+        /// <summary>
+        /// The position of the border.
+        /// </summary>
+        public BorderPosition Position { get; private set; }
+
+        /// <summary>
+        /// Creates a new window border using the specified element and position.
+        /// </summary>
+        /// <param name="position"></param>
+        /// <param name="element"></param>
+        public WindowBorder(BorderPosition position, FrameworkElement element)
+        {
+            if (element == null)
+            {
+                throw new ArgumentNullException("element");
+            }
+
+            Position = position;
+            Element = element;
+        }
+    }
+}
